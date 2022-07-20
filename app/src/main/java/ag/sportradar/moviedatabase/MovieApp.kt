f6533ed34290ee56e98dee5084c50f4e2e4b30cd
c.frank <c.frank@sportradar.com>
2022-07-20 09:09:25 +0200

    Initialized FakeApi in Application class.

--- app/src/main/java/ag/sportradar/moviedatabase/MovieApp.kt
+++ app/src/main/java/ag/sportradar/moviedatabase/MovieApp.kt
@@ -2,5 +2,11 @@ package ag.sportradar.moviedatabase
 
 import android.app.Application
 
-class MovieApp: Application() {
+class MovieApp : Application() {
+
+    val api: OMDbApi = OmDbFakeApi()
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
