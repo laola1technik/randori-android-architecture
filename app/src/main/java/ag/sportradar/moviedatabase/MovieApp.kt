669b100a72f742d384031bc0bedeabfc5d91f527
c.frank <c.frank@sportradar.com>
2022-07-20 09:18:44 +0200

    Creates real api in application class with retrofit.

--- app/src/main/java/ag/sportradar/moviedatabase/MovieApp.kt
+++ app/src/main/java/ag/sportradar/moviedatabase/MovieApp.kt
@@ -5,6 +5,11 @@ import android.app.Application
 class MovieApp : Application() {
 
     val api: OMDbApi = OmDbFakeApi()
+//    val api: OMDbApi = Retrofit.Builder()
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .baseUrl("http://www.omdbapi.com/")
+//        .build()
+//        .create(OMDbApi::class.java)
 
     override fun onCreate() {
         super.onCreate()
